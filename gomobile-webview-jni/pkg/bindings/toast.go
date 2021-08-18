7518bac6496cb4eb2e50a44acd0fa91cd6df01f1
Felicitas Pojtinger <felicitas@pojtinger.com>
2021-08-18 21:37:20 +0200

    feat: Add toast implementation using JNI

--- gomobile-webview-jni/pkg/bindings/toast.go
+++ gomobile-webview-jni/pkg/bindings/toast.go
@@ -6,9 +6,9 @@ package bindings
 import "C"
 import "golang.org/x/mobile/app"
 
-func ShowToast() error {
+func ShowToast(msg string) error {
 	return app.RunOnJVM(func(vm, env, ctx uintptr) error {
-		C.show_toast(C.uintptr_t(vm), C.uintptr_t(env), C.uintptr_t(ctx))
+		C.show_toast(C.uintptr_t(vm), C.uintptr_t(env), C.uintptr_t(ctx), C.CString(msg))
 
 		return nil
 	})
