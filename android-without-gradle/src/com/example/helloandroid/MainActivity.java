bab55f145c04d41285d2e3b07b93cb4e708fdb8b
Felix Pojtinger <felix@pojtinger.com>
2021-08-19 23:25:14 +0200

    build: use modern Android SDK versions

--- android-without-gradle/src/com/example/helloandroid/MainActivity.java
+++ android-without-gradle/src/com/example/helloandroid/MainActivity.java
@@ -13,8 +13,6 @@ public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        getActionBar().hide();
-
         Context context = getApplicationContext();
         super.onCreate(savedInstanceState);
 
