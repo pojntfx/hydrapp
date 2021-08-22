e5547caeb579bfb2b9a57aa2b3a9705f9eb54a26
Felix Pojtinger <felix@pojtinger.com>
2021-08-23 00:58:03 +0200

    feat: Start using simplified non-gomobile JNI API

--- android-without-gomobile/pkg/bindings/toast.go
+++ android-without-gomobile/pkg/bindings/toast.go
@@ -6,12 +6,9 @@ package bindings
 #include "toast.h"
 */
 import "C"
-import "golang.org/x/mobile/app"
 
-func ShowToast(msg string) error {
-	return app.RunOnJVM(func(vm, env, ctx uintptr) error {
-		C.show_toast(C.uintptr_t(vm), C.uintptr_t(env), C.uintptr_t(ctx), C.CString(msg))
+func ShowToast(env, ctx uintptr, msg string) error {
+	C.show_toast(C.uintptr_t(env), C.uintptr_t(ctx), C.CString(msg))
 
-		return nil
-	})
+	return nil
 }
