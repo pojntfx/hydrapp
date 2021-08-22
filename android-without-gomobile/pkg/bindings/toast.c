48e71c704acf59b3056bb38b86332bfa56faec08
Felicitas Pojtinger <felicitas@pojtinger.com>
2021-08-23 00:58:03 +0200

    feat: Start using simplified non-gomobile JNI API

--- android-without-gomobile/pkg/bindings/toast.c
+++ android-without-gomobile/pkg/bindings/toast.c
@@ -1,18 +1,9 @@
 #include <jni.h>
 
-void show_toast(uintptr_t java_vm, uintptr_t jni_env, uintptr_t ctx,
-                char *msg) {
-  // Cast the Java VM, env and context
-  JavaVM *vm = (JavaVM *)java_vm;
+void show_toast(uintptr_t jni_env, uintptr_t ctx, char *msg) {
+  // Cast the Java env and context
   JNIEnv *env = (JNIEnv *)jni_env;
   jobject context = (jobject)ctx;
-
-  // Get the looper functions
-  jclass looper_cls = (*env)->FindClass(env, "android/os/Looper");
-  jmethodID looper_cls_prepare =
-      (*env)->GetStaticMethodID(env, looper_cls, "prepare", "()V");
-  jmethodID lopper_cls_loop =
-      (*env)->GetStaticMethodID(env, looper_cls, "loop", "()V");
 
   // Get the toast functions
   jclass toast_cls = (*env)->FindClass(env, "android/widget/Toast");
@@ -20,9 +11,6 @@ void show_toast(uintptr_t java_vm, uintptr_t jni_env, uintptr_t ctx,
       (*env)->GetStaticMethodID(env, toast_cls, "makeText",
                                 "(Landroid/content/Context;Ljava/lang/"
                                 "CharSequence;I)Landroid/widget/Toast;");
-
-  // Lock the UI thread
-  (*env)->CallStaticVoidMethod(env, looper_cls, looper_cls_prepare);
 
   // Get the message to display
   jobject msg_obj = (*env)->NewStringUTF(env, msg);
@@ -36,7 +24,4 @@ void show_toast(uintptr_t java_vm, uintptr_t jni_env, uintptr_t ctx,
   jmethodID toast_obj_class_show =
       (*env)->GetMethodID(env, toast_obj_class, "show", "()V");
   (*env)->CallVoidMethod(env, toast_obj, toast_obj_class_show);
-
-  // Unlock the UI thread
-  //   (*env)->CallStaticVoidMethod(env, looper_cls, lopper_cls_loop);
 }
--- android-without-gomobile/pkg/main/main.go
+++ android-without-gomobile/pkg/main/main.go
@@ -2,11 +2,23 @@
 
 package main
 
-import "github.com/pojntfx/hydrapp/example/pkg/bindings"
+/*
+#cgo LDFLAGS: -llog
 
-//export show_toast
-func ShowToast(msg string) error {
-	return bindings.ShowToast(msg)
+#include <jni.h>
+*/
+import "C"
+import (
+	"unsafe"
+
+	"github.com/pojntfx/hydrapp/example/pkg/bindings"
+)
+
+//export Java_com_pojtinger_gomobilefreeexperiments_MainActivity_showToast
+func Java_com_pojtinger_gomobilefreeexperiments_MainActivity_showToast(env *C.JNIEnv, _ C.jobject, ctx C.jobject, msg C.jstring) {
+	if err := bindings.ShowToast(uintptr(unsafe.Pointer(env)), uintptr(ctx), "Hello, world!"); err != nil {
+		panic(err)
+	}
 }
 
 func main() {}
