d7106e07c56e63f6fd4d12f456aa0b75b75d6f8e
Felicitas Pojtinger <felicitas@pojtinger.com>
2021-08-24 23:14:54 +0200

    feat: add access to plain Java activity from `GoLoop`

--- android-native-activity/pkg/main/main.h
+++ android-native-activity/pkg/main/main.h
@@ -1,3 +1,7 @@
-#include "android/native_activity.h"
+#include <jni.h>
 
-void GoLoop(ANativeActivity *activity);
+void Java_com_example_app_MainActivity_GoLoop(JNIEnv *env, jobject activity);
+
+void GoLoop(JNIEnv *env, jobject activity);
+
+void show_toast(JNIEnv *env, jobject activity);
