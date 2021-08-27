8e5f85759cd508c73a339a38b25995ca73e74c0c
Felicitas Pojtinger <felicitas@pojtinger.com>
2021-08-27 22:59:19 +0200

    feat: Add custom enhanced Android WebView with video, mic, file access etc. support

--- android-integrated-webserver/MainActivity.java
+++ android-integrated-webserver/MainActivity.java
@@ -2,93 +2,123 @@ package com.pojtinger.felicitas.integratedWebserverExample;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
-import android.widget.FrameLayout;
+import android.view.KeyEvent;
+import android.webkit.PermissionRequest;
+import android.webkit.ValueCallback;
+import android.webkit.WebChromeClient;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.LinearLayout;
-import im.delight.android.webview.AdvancedWebView;
 
-public class MainActivity extends Activity implements AdvancedWebView.Listener {
+public class MainActivity extends Activity {
     static {
         System.loadLibrary("backend");
     }
 
     private native String LaunchBackend();
 
-    private AdvancedWebView view;
+    private ValueCallback<Uri[]> fileChooserCallback;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        LinearLayout layout = new LinearLayout(this);
-        layout.setLayoutParams(new FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT,
-                FrameLayout.LayoutParams.MATCH_PARENT));
+        Uri home = Uri.parse(LaunchBackend());
 
-        view = new AdvancedWebView(this);
-        view.setListener(this, this);
+        WebView view = new WebView(this);
         view.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,
                 LinearLayout.LayoutParams.MATCH_PARENT));
 
-        String url = LaunchBackend();
-        view.loadUrl(url);
-        layout.addView(view);
+        WebSettings settings = view.getSettings();
+        settings.setAllowContentAccess(true);
+        settings.setAllowFileAccess(true);
+        settings.setDatabaseEnabled(true);
+        settings.setDomStorageEnabled(true);
+        settings.setGeolocationEnabled(true);
+        settings.setJavaScriptCanOpenWindowsAutomatically(true);
+        settings.setJavaScriptEnabled(true);
+        settings.setLoadsImagesAutomatically(true);
+        settings.setMediaPlaybackRequiresUserGesture(false);
+        settings.setSupportMultipleWindows(true);
 
-        setContentView(layout);
-    }
+        view.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView vw, WebResourceRequest request) {
+                if (request.getUrl().toString().contains(home.getHost())) {
+                    vw.loadUrl(request.getUrl().toString());
+                } else {
+                    Intent intent = new Intent(Intent.ACTION_VIEW, request.getUrl());
+                    vw.getContext().startActivity(intent);
+                }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        view.onResume();
-    }
+                return true;
+            }
+        });
+        view.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public void onPermissionRequest(final PermissionRequest request) {
+                request.grant(request.getResources());
+            }
 
-    @Override
-    protected void onPause() {
-        view.onPause();
-        super.onPause();
-    }
+            @Override
+            public boolean onShowFileChooser(WebView vw, ValueCallback<Uri[]> filePathCallback,
+                    FileChooserParams fileChooserParams) {
+                if (fileChooserCallback != null) {
+                    fileChooserCallback.onReceiveValue(null);
+                }
+                fileChooserCallback = filePathCallback;
 
-    @Override
-    protected void onDestroy() {
-        view.onDestroy();
-        super.onDestroy();
+                Intent selectionIntent = new Intent(Intent.ACTION_GET_CONTENT);
+                selectionIntent.addCategory(Intent.CATEGORY_OPENABLE);
+                selectionIntent.setType("*/*");
+
+                Intent chooserIntent = new Intent(Intent.ACTION_CHOOSER);
+                chooserIntent.putExtra(Intent.EXTRA_INTENT, selectionIntent);
+                startActivityForResult(chooserIntent, 0);
+
+                return true;
+            }
+        });
+        view.setOnKeyListener((v, keyCode, event) -> {
+            WebView vw = (WebView) v;
+            if (event.getAction() == KeyEvent.ACTION_DOWN && keyCode == KeyEvent.KEYCODE_BACK && vw.canGoBack()) {
+                vw.goBack();
+
+                return true;
+            }
+
+            return false;
+        });
+        view.setDownloadListener((uri, userAgent, contentDisposition, mimetype, contentLength) -> handleURI(uri));
+        view.setOnLongClickListener(v -> {
+            handleURI(((WebView) v).getHitTestResult().getExtra());
+
+            return true;
+        });
+
+        view.loadUrl(home.toString());
+
+        setContentView(view);
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
         super.onActivityResult(requestCode, resultCode, intent);
-        view.onActivityResult(requestCode, resultCode, intent);
+
+        fileChooserCallback.onReceiveValue(new Uri[] { Uri.parse(intent.getDataString()) });
+        fileChooserCallback = null;
     }
 
-    @Override
-    public void onBackPressed() {
-        if (!view.onBackPressed()) {
-            return;
+    private void handleURI(String uri) {
+        if (uri != null) {
+            Intent i = new Intent(Intent.ACTION_VIEW);
+            i.setData(Uri.parse(uri.replaceFirst("^blob:", "")));
+
+            startActivity(i);
         }
-
-        super.onBackPressed();
     }
-
-    @Override
-    public void onPageStarted(String url, Bitmap favicon) {
-    }
-
-    @Override
-    public void onPageFinished(String url) {
-    }
-
-    @Override
-    public void onPageError(int errorCode, String description, String failingUrl) {
-    }
-
-    @Override
-    public void onDownloadRequested(String url, String suggestedFilename, String mimeType, long contentLength,
-            String contentDisposition, String userAgent) {
-    }
-
-    @Override
-    public void onExternalPageRequest(String url) {
-    }
-
 }
