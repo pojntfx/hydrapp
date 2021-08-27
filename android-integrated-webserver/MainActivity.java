1fe9774725a9af2c2a54b65d600fc25d814d430f
Felicitas Pojtinger <felicitas@pojtinger.com>
2021-08-27 18:30:57 +0200

    feat: Use `AdvancedWebView`

--- android-integrated-webserver/MainActivity.java
+++ android-integrated-webserver/MainActivity.java
@@ -1,17 +1,21 @@
 package com.pojtinger.felicitas.integratedWebserverExample;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
-import android.webkit.WebView;
 import android.widget.FrameLayout;
 import android.widget.LinearLayout;
+import im.delight.android.webview.AdvancedWebView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements AdvancedWebView.Listener {
     static {
         System.loadLibrary("backend");
     }
 
     private native String LaunchBackend();
+
+    private AdvancedWebView view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,10 +25,10 @@ public class MainActivity extends Activity {
         layout.setLayoutParams(new FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT,
                 FrameLayout.LayoutParams.MATCH_PARENT));
 
-        WebView view = new WebView(this);
+        view = new AdvancedWebView(this);
+        view.setListener(this, this);
         view.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,
                 LinearLayout.LayoutParams.MATCH_PARENT));
-        view.getSettings().setJavaScriptEnabled(true);
 
         String url = LaunchBackend();
         view.loadUrl(url);
@@ -32,4 +36,59 @@ public class MainActivity extends Activity {
 
         setContentView(layout);
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        view.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        view.onPause();
+        super.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        view.onDestroy();
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+        view.onActivityResult(requestCode, resultCode, intent);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (!view.onBackPressed()) {
+            return;
+        }
+
+        super.onBackPressed();
+    }
+
+    @Override
+    public void onPageStarted(String url, Bitmap favicon) {
+    }
+
+    @Override
+    public void onPageFinished(String url) {
+    }
+
+    @Override
+    public void onPageError(int errorCode, String description, String failingUrl) {
+    }
+
+    @Override
+    public void onDownloadRequested(String url, String suggestedFilename, String mimeType, long contentLength,
+            String contentDisposition, String userAgent) {
+    }
+
+    @Override
+    public void onExternalPageRequest(String url) {
+    }
+
 }
