2c1f044459f14aeaf0bf8f53ef93b58a48a0d98e
Felicitas Pojtinger <felicitas@pojtinger.com>
2022-09-26 12:34:56 +0200

    feat: Add RPM builder

---
+++ example/cmd/hydrapp-build-rpm/entrypoint.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+set -e
+
+# Setup GPG
+echo "${GPG_KEY_PASSWORD}" | base64 -d >'/tmp/gpg-pass'
+mkdir -p "${HOME}/.gnupg"
+cat >"${HOME}/.gnupg/gpg.conf" <<EOT
+yes
+passphrase-file /tmp/gpg-pass
+pinentry-mode loopback
+EOT
+
+echo "${GPG_KEY_CONTENT}" | base64 -d >'/tmp/private.gpg'
+gpg --import /tmp/private.gpg
+
+echo "%_signature gpg
+%_gpg_name ${GPG_KEY_ID}" >"${HOME}/.rpmmacros"
+
+# Build tarball and source package
+export PACKAGE="${APP_ID}-${PACKAGE_VERSION}"
+export SUFFIX="${PACKAGE_SUFFIX}"
+export SPEC="${APP_ID}.spec"
+
+rpmdev-setuptree
+
+export TARBALL="${HOME}/rpmbuild/SOURCES/${PACKAGE}.tar.gz"
+export DSC="${HOME}/rpmbuild/SRPMS/${PACKAGE}-${SUFFIX}.src.rpm"
+
+tar -cvzf "${TARBALL}" --exclude out --transform "s,^,${PACKAGE}/," .
+rpmbuild -bs "${SPEC}"
+
+rpmlint "${DSC}"
+
+# Build chroot
+for TARGET in ${TARGETS}; do
+  export DIST="$(cut -d'|' -f1 <<<"${TARGET}")"
+  export ARCHITECTURES="$(cut -d'|' -f2 <<<"${TARGET}")"
+
+  for ARCH in ${ARCHITECTURES}; do
+    mock -r "${DIST}-${ARCH}" "${DSC}" --enable-network
+
+    rpmlint "/var/lib/mock/${DIST}-${ARCH}/result/*.rpm"
+
+    mkdir -p "/dst/${DIST}"
+    cp "/var/lib/mock/${DIST}-${ARCH}/result/"*.rpm "/dst/${DIST}"
+
+    rpm --addsign "/dst/${DIST}"/*.rpm
+
+    createrepo "/dst/${DIST}"
+
+    gpg --detach-sign --armor "/dst/${DIST}/repodata/repomd.xml"
+
+    gpg --output "/dst/${DIST}/repodata/repo.asc" --armor --export
+
+    # Add repo file
+    echo "[hydrapp-repo]
+name=Hydrapp YUM repo
+baseurl=${BASE_URL}/${DIST}
+enabled=1
+gpgcheck=1
+gpgkey=${BASE_URL}/${DIST}/repodata/repo.asc" >"/dst/${DIST}/repodata/hydrapp.repo"
+  done
+done
