3ec2662ab354c26dd3b81e5b1969e03e51d453e0
Felicitas Pojtinger <felicitas@pojtinger.com>
2022-09-30 23:57:49 +0200

    feat: Start implementation of CGo support for Windows

--- example/cmd/hydrapp-build-msi/entrypoint.sh
+++ example/cmd/hydrapp-build-msi/entrypoint.sh
@@ -14,6 +14,11 @@ EOT
 echo "${GPG_KEY_CONTENT}" | base64 -d >'/tmp/private.gpg'
 gpg --import /tmp/private.gpg
 
+# Install MSYS2 packages
+if [ "${MSYS2PACKAGES}" != "" ]; then
+  wine64 bash.exe -c "pacman --noconfirm --ignore pacman --needed -S ${MSYS2PACKAGES}"
+fi
+
 # Create icons
 convert -resize 'x16' -gravity 'center' -crop '16x16+0+0' -flatten -colors '256' -background 'transparent' 'icon.png' '/dst/icon.ico'
 
@@ -34,11 +39,25 @@ for ARCH in ${ARCHITECTURES}; do
     export DEBARCH="aarch64"
   fi
 
-  GOFLAGS='-tags=selfupdate' go build -o "/dst/${APP_ID}.${GOOS}-${DEBARCH}.exe" .
-  gpg --detach-sign --armor "/dst/${APP_ID}.${GOOS}-${DEBARCH}.exe"
+  if [ "${ARCH}" = "amd64" ]; then
+    for UNIXPATH in $(find . -type f); do
+      export WINDOWSPATH="$(sed s,/,\\\\,g <<<$(echo ${UNIXPATH} | sed s,^./,,g))"
+      export UUID="$(uuid)"
 
-  wixl -o "/dst/${APP_ID}.${GOOS}-${DEBARCH}.msi" <(sed "s@Source=\"${APP_ID}.exe\"@Source=\"/dst/${APP_ID}.${GOOS}-${DEBARCH}.exe\"@g" ${APP_ID}.wxl | sed 's@SourceFile="icon.ico"@SourceFile="/dst/icon.ico"@g' | sed 's@Source="icon.ico"@Source="/dst/icon.ico"@g')
-  gpg --detach-sign --armor "/dst/${APP_ID}.${GOOS}-${DEBARCH}.msi"
+      echo "<File Id=\"${UUID}\" Source=\"${WINDOWSPATH}\" KeyPath=\"yes\" DiskId=\"1\" />"
+
+      if [[ "${UNIXPATH}" == *${APP_ID} ]]; then
+        echo "<Shortcut Id=\"${UUID}\" Advertise=\"yes\" Icon=\"icon.ico\" Name=\"${APP_NAME}\" Directory=\"ProgramMenuFolder\" WorkingDirectory=\"INSTALLDIR\" Description=\"${APP_NAME}\" />"
+        echo "<Shortcut Id=\"${UUID}\" Advertise=\"yes\" Icon=\"icon.ico\" Name=\"${APP_NAME}\" Directory=\"DesktopFolder\" WorkingDirectory=\"INSTALLDIR\" Description=\"${APP_NAME}\" />"
+      fi
+    done
+  else
+    GOFLAGS='-tags=selfupdate' go build -o "/dst/${APP_ID}.${GOOS}-${DEBARCH}.exe" .
+    gpg --detach-sign --armor "/dst/${APP_ID}.${GOOS}-${DEBARCH}.exe"
+
+    wixl -o "/dst/${APP_ID}.${GOOS}-${DEBARCH}.msi" <(sed "s@Source=\"${APP_ID}.exe\"@Source=\"/dst/${APP_ID}.${GOOS}-${DEBARCH}.exe\"@g" ${APP_ID}.wxl | sed 's@SourceFile="icon.ico"@SourceFile="/dst/icon.ico"@g' | sed 's@Source="icon.ico"@Source="/dst/icon.ico"@g')
+    gpg --detach-sign --armor "/dst/${APP_ID}.${GOOS}-${DEBARCH}.msi"
+  fi
 done
 
 rm '/dst/icon.ico'
--- example/cmd/hydrapp-build-msi/main.go
+++ example/cmd/hydrapp-build-msi/main.go
@@ -20,9 +20,11 @@ func main() {
 	pull := flag.Bool("pull", true, "Whether to pull the image or not")
 	dst := flag.String("dst", filepath.Join(pwd, "out", "msi"), "Output directory")
 	appID := flag.String("app-id", "com.pojtinger.felicitas.hydrapp.example", "Windows app ID to use")
+	appName := flag.String("app-name", "Hydrapp Example", "Human-readable name for the app")
 	gpgKeyContent := flag.String("gpg-key-content", "", "base64-encoded GPG key contents")
 	gpgKeyPassword := flag.String("gpg-key-password", "", " base64-encoded password for the GPG key")
-	architectures := flag.String("architectures", "386 amd64 arm arm64", "Space-separated list of architectures to build for")
+	architectures := flag.String("architectures", "386 amd64 arm arm64", "Space-separated list of architectures to build for. CGo is only supported for amd64.")
+	packages := flag.String("packages", "", "Space-separated list of MSYS2 packages to install. Only supported for amd63")
 
 	flag.Parse()
 
@@ -44,9 +46,11 @@ func main() {
 		*dst,
 		map[string]string{
 			"APP_ID":           *appID,
+			"APP_NAME":         *appName,
 			"GPG_KEY_CONTENT":  *gpgKeyContent,
 			"GPG_KEY_PASSWORD": *gpgKeyPassword,
 			"ARCHITECTURES":    *architectures,
+			"MSYS2PACKAGES":    *packages,
 		},
 	); err != nil {
 		panic(err)
