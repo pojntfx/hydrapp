5d1513f221781c700961099d0dcdda44898119ff
Felicitas Pojtinger <felicitas@pojtinger.com>
2022-10-02 01:20:33 +0200

    feat: Add proper shortcut to Wix manifest

--- example/cmd/hydrapp-build-msi/entrypoint.sh
+++ example/cmd/hydrapp-build-msi/entrypoint.sh
@@ -47,6 +47,7 @@ for ARCH in ${ARCHITECTURES}; do
     GOPATH='/root/.wine/drive_c/go' go mod download -x
 
     cp -r . '/root/.wine/drive_c/users/root/Documents/go-workspace'
+    rm -rf '/root/.wine/drive_c/users/root/Documents/go-workspace/out'
 
     wine64 bash.exe -c "export PATH=$PATH:/mingw64/bin GOPATH=/c/go GOROOT=/mingw64/lib/go TMP=/c/tmp TEMP=/c/tmp GOARCH=amd64 CGO_ENABLED=1 && cd /c/users/root/Documents/go-workspace && go build -buildvcs=false -ldflags='-linkmode=external' -x -v -o out/${APP_ID}.${GOOS}-${DEBARCH}.exe ."
 
@@ -61,24 +62,17 @@ for ARCH in ${ARCHITECTURES}; do
   cd '/tmp/out'
 
   # Prepare WiX installer
-  export FILES=""
   export STARTID=""
   for UNIXPATH in $(find . -type f); do
-    export WINDOWSPATH="$(sed s,/,\\\\,g <<<$(echo ${UNIXPATH} | sed s,^./,,g))"
     export UUID="$(uuid)"
+
+    echo "<File Id=\"${UUID}\" Name=\"${UNIXPATH}\" Source=\"${UNIXPATH}\" DiskId=\"1\" />\n" >>'/tmp/files'
 
     if [[ "${UNIXPATH}" == *${APP_ID}.${GOOS}-${DEBARCH}.exe ]]; then
       export STARTID="${UUID}"
-      export FILES="${FILES}<File Id=\"${UUID}\" Source=\"${WINDOWSPATH}\" DiskId=\"1\" />"
-      export FILES="${FILES}<Shortcut Id=\"${UUID}\" Advertise=\"yes\" Icon=\"icon.ico\" Name=\"${APP_NAME}\" Directory=\"ProgramMenuFolder\" WorkingDirectory=\"INSTALLDIR\" Description=\"${APP_NAME}\" />"
-      export FILES="${FILES}<Shortcut Id=\"${UUID}\" Advertise=\"yes\" Icon=\"icon.ico\" Name=\"${APP_NAME}\" Directory=\"DesktopFolder\" WorkingDirectory=\"INSTALLDIR\" Description=\"${APP_NAME}\" />"
-    else
-      export FILES="${FILES}<File Id=\"${UUID}\" Source=\"${WINDOWSPATH}\" DiskId=\"1\" />"
     fi
   done
 
-  cat <(cat "${BASEDIR}/${APP_ID}.wxl" | perl -p -e 's+<HydrappFiles />+$ENV{FILES}+g' | perl -p -e 's+{{ StartID }}+$ENV{STARTID}+g')
-
-  wixl -v -o "/dst/${APP_ID}.${GOOS}-${DEBARCH}.msi" <(cat "${BASEDIR}/${APP_ID}.wxl" | perl -p -e 's+<HydrappFiles />+$ENV{FILES}+g' | perl -p -e 's+{{ StartID }}+$ENV{STARTID}+g')
+  wixl -v -o "/dst/${APP_ID}.${GOOS}-${DEBARCH}.msi" <(cat "${BASEDIR}/${APP_ID}.wxl" | perl -p -e 'use File::Slurp; my $text = read_file("/tmp/files"); s+<HydrappFiles />+$text+g' | perl -p -e 's+{{ StartID }}+$ENV{STARTID}+g')
   gpg --detach-sign --armor "/dst/${APP_ID}.${GOOS}-${DEBARCH}.msi"
 done
