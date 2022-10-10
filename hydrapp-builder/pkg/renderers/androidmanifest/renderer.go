6fe35a89a1ab729c867411f79e169dde77309598
Felicitas Pojtinger <felicitas@pojtinger.com>
2022-10-11 00:12:36 +0200

    refactor: Re-use rendering logic in generic renderer

--- hydrapp-builder/pkg/renderers/androidmanifest/renderer.go
+++ hydrapp-builder/pkg/renderers/androidmanifest/renderer.go
@@ -1,51 +1,22 @@
 package androidmanifest
 
 import (
-	"bytes"
 	_ "embed"
-	"text/template"
+
+	"github.com/pojntfx/hydrapp/hydrapp-builder/pkg/renderers"
 )
 
-const (
-	path = "AndroidManifest.xml"
-)
-
-var (
-	//go:embed AndroidManifest.xml
-	tpl string
-)
+//go:embed AndroidManifest.xml
+var template string
 
 type data struct {
 	AppID   string
 	AppName string
 }
 
-type Renderer struct {
-	data data
-}
-
 func NewRenderer(
 	appID string,
 	appName string,
-) *Renderer {
-	return &Renderer{
-		data{
-			appID,
-			appName,
-		},
-	}
-}
-
-func (r *Renderer) Render() (filePath string, fileContent string, err error) {
-	t, err := template.New(path).Parse(tpl)
-	if err != nil {
-		return "", "", err
-	}
-
-	buf := bytes.NewBuffer([]byte{})
-	if err := t.Execute(buf, r.data); err != nil {
-		return "", "", err
-	}
-
-	return path, buf.String(), err
+) *renderers.Renderer[data] {
+	return renderers.NewRenderer("AndroidManifest.xml", template, data{appID, appName})
 }
--- hydrapp-builder/pkg/renderers/renderer.go
+++ hydrapp-builder/pkg/renderers/renderer.go
@@ -1,5 +1,38 @@
 package renderers
 
-type Renderer interface {
-	Render() (filePath string, fileContent string, err error)
+import (
+	"bytes"
+	"text/template"
+)
+
+type Renderer[T interface{}] struct {
+	filePath string
+	template string
+	data     T
+}
+
+func NewRenderer[T interface{}](
+	filePath string,
+	template string,
+	data T,
+) *Renderer[T] {
+	return &Renderer[T]{
+		filePath,
+		template,
+		data,
+	}
+}
+
+func (r *Renderer[T]) Render() (filePath string, fileContent string, err error) {
+	t, err := template.New(r.filePath).Parse(r.template)
+	if err != nil {
+		return "", "", err
+	}
+
+	buf := bytes.NewBuffer([]byte{})
+	if err := t.Execute(buf, r.data); err != nil {
+		return "", "", err
+	}
+
+	return r.filePath, buf.String(), err
 }
