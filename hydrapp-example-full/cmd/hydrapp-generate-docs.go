25816f39e01eb8f52b4be132a430f3b6eb661e50
Felicitas Pojtinger <felicitas@pojtinger.com>
2022-11-08 15:32:24 +0100

    feat: Start implementation of installation docs generator

---
+++ hydrapp-example-full/cmd/hydrapp-generate-docs.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"bytes"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"text/template"
+
+	_ "embed"
+
+	"github.com/pojntfx/hydrapp/hydrapp-builder/pkg/builders"
+	cconfig "github.com/pojntfx/hydrapp/hydrapp-builder/pkg/config"
+)
+
+//go:embed INSTALLATION.md.tpl
+var installationTemplate string
+
+type installationData struct {
+	AndroidRepoURL  string
+	AppName         string
+	MacOSBinaryURL  string
+	MacOSBinaryName string
+}
+
+func main() {
+	config := flag.String("config", "hydrapp.yaml", "Config file to use")
+	branchID := flag.String("branch-id", "", `Branch ID to build the app as, i.e. main (for an app ID like "myappid.main" and baseURL like "mybaseurl/main"`)
+	branchName := flag.String("branch-name", "", `Branch name to build the app as, i.e. Main (for an app name like "myappname (Main)"`)
+
+	flag.Parse()
+
+	content, err := ioutil.ReadFile(*config)
+	if err != nil {
+		panic(err)
+	}
+
+	cfg, err := cconfig.Parse(content)
+	if err != nil {
+		panic(err)
+	}
+
+	t, err := template.
+		New("INSTALLATION.md").
+		Parse(installationTemplate)
+	if err != nil {
+		panic(err)
+	}
+
+	appName := builders.GetAppNameForBranch(cfg.App.Name, *branchName)
+	macOSBinaryName := builders.GetAppIDForBranch(cfg.App.ID, *branchID) + ".darwin.dmg"
+
+	buf := bytes.NewBuffer([]byte{})
+	if err := t.Execute(buf, installationData{
+		AppName:         appName,
+		AndroidRepoURL:  cfg.App.BaseURL + builders.GetPathForBranch(cfg.APK.Path, *branchID),
+		MacOSBinaryURL:  cfg.App.BaseURL + builders.GetPathForBranch(cfg.DMG.Path, *branchID) + "/" + macOSBinaryName,
+		MacOSBinaryName: macOSBinaryName,
+	}); err != nil {
+		panic(err)
+	}
+
+	fmt.Println(buf.String())
+}
