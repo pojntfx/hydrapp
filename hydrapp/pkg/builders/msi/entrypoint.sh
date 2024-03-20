c8627324b7bac58d67bccd4e0f3f7548f34d1a43
Felicitas Pojtinger <felicitas@pojtinger.com>
2024-03-20 02:39:01 +0100

    build: Set `GIT_CONFIG` manually for Windows builds

--- hydrapp/pkg/builders/msi/entrypoint.sh
+++ hydrapp/pkg/builders/msi/entrypoint.sh
@@ -66,7 +66,7 @@ if [ "${ARCHITECTURE}" = "amd64" ]; then
   cp -r . '/root/.wine/drive_c/users/root/Documents/go-workspace'
   rm -rf '/root/.wine/drive_c/users/root/Documents/go-workspace/out'
 
-  wine64 bash.exe -c "export PATH=$PATH:/ucrt64/bin:/msys64/usr/bin GOPATH=/c/go GOROOT=/ucrt64/lib/go TMP=/c/tmp TEMP=/c/tmp GOARCH=amd64 CGO_ENABLED=1 GOPROXY='https://proxy.golang.org,direct' GOFLAGS=${GOFLAGS} && cd /c/users/root/Documents/go-workspace && git config --add safe.directory '*' && go build -ldflags='-linkmode=external -H=windowsgui' -x -v -o out/${APP_ID}.${GOOS}-${DEBARCH}.exe ${GOMAIN}"
+  wine64 bash.exe -c "export PATH=$PATH:/ucrt64/bin:/msys64/usr/bin GOPATH=/c/go GOROOT=/ucrt64/lib/go TMP=/c/tmp TEMP=/c/tmp GOARCH=amd64 CGO_ENABLED=1 GOPROXY='https://proxy.golang.org,direct' GOFLAGS=${GOFLAGS} GIT_CONFIG=/c/tmp/.gitconfig && touch /c/tmp/.gitconfig && cd /c/users/root/Documents/go-workspace && git config --global --add safe.directory '*' && go build -ldflags='-linkmode=external -H=windowsgui' -x -v -o out/${APP_ID}.${GOOS}-${DEBARCH}.exe ${GOMAIN}"
 
   # Copy binaries to staging directory
   yes | cp -rf /root/.wine/drive_c/users/root/Documents/go-workspace/out/* '/tmp/out'
