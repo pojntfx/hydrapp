46850ed75527a49f9ac991d811c9c827e8190129
Felicitas Pojtinger <felicitas@pojtinger.com>
2024-06-07 18:35:15 -0700

    feat: Implement browser download dialog action to "no supported browser found" dialog

--- hydrapp/pkg/utils/handle_no_supported_browser_found.go
+++ hydrapp/pkg/utils/handle_no_supported_browser_found.go
@@ -3,8 +3,18 @@ package utils
 import (
 	"errors"
 	"fmt"
+	"os/exec"
+	"runtime"
 
 	"github.com/ncruces/zenity"
+)
+
+const (
+	browserDownloadLink = "https://github.com/pojntfx/hydrapp#which-browsers-are-supported"
+)
+
+var (
+	ErrNoBrowserOpenMethodFound = errors.New("no method to open a browser found")
 )
 
 func HandleNoSupportedBrowserFound(
@@ -47,6 +57,8 @@ without success. Would you like to manually configure a browser?`,
 			); err != nil {
 				if errors.Is(zenity.ErrCanceled, err) {
 					return err
+				} else {
+					return fmt.Errorf("could not display dialog: %v", err)
 				}
 			}
 
@@ -54,11 +66,51 @@ without success. Would you like to manually configure a browser?`,
 
 			return nil
 		} else {
-			return err
+			return fmt.Errorf("could not display dialog: %v", err)
 		}
 	}
 
-	// TODO: Open browser download selection screen, show "ok"-only modal asking the user to press ok once they have installed the browser, return nil causing it to re-try
+	switch runtime.GOOS {
+	case "windows":
+		powerShellBinary, err := exec.LookPath("pwsh.exe")
+		if err != nil {
+			powerShellBinary = "powershell.exe"
+		}
+
+		if output, err := exec.Command(powerShellBinary, `Start-Process`, browserDownloadLink).CombinedOutput(); err != nil {
+			return fmt.Errorf("could not open browser with output: %s: %v", output, err)
+		}
+
+	case "darwin":
+		if output, err := exec.Command("open", browserDownloadLink).CombinedOutput(); err != nil {
+			return fmt.Errorf("could not open browser with output: %s: %v", output, err)
+		}
+
+	default:
+		// Open link with `xdg-open`
+		if output, err := exec.Command("xdg-open", browserDownloadLink).CombinedOutput(); err != nil {
+			return fmt.Errorf("could not open browser with output: %s: %v", output, err)
+		}
+
+		// Open link with `xdg-open`
+		if xdgOpen, err := exec.LookPath("xdg-open"); err == nil {
+			if output, err := exec.Command(xdgOpen, browserDownloadLink).CombinedOutput(); err != nil {
+				return fmt.Errorf("could not open browser with output: %s: %v", output, err)
+			}
+		} else {
+			// Open link with `open` (i.e. FreeBSD and other UNIXes)
+			open, err := exec.LookPath("open")
+			if err != nil {
+				return fmt.Errorf("%v: %w", ErrNoBrowserOpenMethodFound, err)
+			}
+
+			if output, err := exec.Command(open, browserDownloadLink).CombinedOutput(); err != nil {
+				return fmt.Errorf("could not open browser with output: %s: %v", output, err)
+			}
+		}
+	}
+
+	// TODO: Show "ok"-only modal asking the user to press ok once they have installed the browser, return nil causing it to re-try
 
 	return nil
 }
