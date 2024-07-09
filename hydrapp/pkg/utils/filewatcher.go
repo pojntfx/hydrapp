7aec7c5e266e2eab424b72971b2a1a4f443e134b
Felicitas Pojtinger <felicitas@pojtinger.com>
2024-07-08 23:45:07 -0700

    fix: Remove data race between checking for path existance and adding the path
    Signed-off-by: Felicitas Pojtinger <felicitas@pojtinger.com>

--- hydrapp/pkg/utils/filewatcher.go
+++ hydrapp/pkg/utils/filewatcher.go
@@ -14,38 +14,38 @@ var (
 )
 
 func WaitForFileRemoval(path string) error {
-	if _, err := os.Stat(path); err == nil {
-		// Wait until browser has exited
-		watcher, err := fsnotify.NewWatcher()
-		if err != nil {
-			return errors.Join(ErrCouldNotStartFileWatcher, err)
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		return errors.Join(ErrCouldNotStartFileWatcher, err)
+	}
+	defer watcher.Close()
+
+	// Wait until browser has exited
+	if err = watcher.Add(path); err != nil {
+		if errors.Is(err, os.ErrNotExist) {
+			return nil
 		}
-		defer watcher.Close()
 
-		if err = watcher.Add(path); err != nil {
-			return errors.Join(ErrCouldNotAddPathToFileWatcher, err)
-		}
-
-		for {
-			select {
-			case event, ok := <-watcher.Events:
-				if !ok {
-					return nil
-				}
-
-				if event.Op&fsnotify.Remove == fsnotify.Remove {
-					return nil
-				}
-
-			case err, ok := <-watcher.Errors:
-				if !ok {
-					return nil
-				}
-
-				return errors.Join(ErrCouldNotWatchFile, err)
-			}
-		}
+		return errors.Join(ErrCouldNotAddPathToFileWatcher, err)
 	}
 
-	return nil
+	for {
+		select {
+		case event, ok := <-watcher.Events:
+			if !ok {
+				return nil
+			}
+
+			if event.Op&fsnotify.Remove == fsnotify.Remove {
+				return nil
+			}
+
+		case err, ok := <-watcher.Errors:
+			if !ok {
+				return nil
+			}
+
+			return errors.Join(ErrCouldNotWatchFile, err)
+		}
+	}
 }
