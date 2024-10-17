f0e05708c30d1ee8e2d53ee507b1d43224fb489f
Felicitas Pojtinger <felicitas@pojtinger.com>
2024-10-17 16:44:52 -0700

    fix: Use `var` for self-updater variables instead of `const`
    Signed-off-by: Felicitas Pojtinger <felicitas@pojtinger.com>

--- hydrapp/pkg/ui/constants.go
+++ hydrapp/pkg/ui/constants.go
@@ -1,6 +1,8 @@
 package ui
 
-const (
+// This needs to be `var`, not `const`, else Go can't overwrite them with
+// `-ldflags="-X github.com/pojntfx/hydrapp/hydrapp/pkg/ui.SelfUpdaterBranchTimestampRFC3339=2024-10-09T23:14:53-07:00"` etc.
+var (
 	SelfUpdaterBranchTimestampRFC3339 = "" // Set by compiler
 	SelfUpdaterBranchID               = "" // Set by compiler
 	SelfUpdaterPackageType            = "" // Set by compiler
